7946f31278dd91e200b016591eca318782e85c36
Evgeny Golyshev <eugulixes@gmail.com>
2019-11-30 15:39:19 +0300

    Remove get_qemu_emulation_binary

--- pieman/pieman/toolset_modules/qemu_user_static.py
+++ pieman/pieman/toolset_modules/qemu_user_static.py
@@ -13,6 +13,10 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+"""Toolset module intended for fetching qemu-user-static 3.1 from Ubuntu 19.04
+"Disco Dingo".
+"""
+
 FLAVOURS_ENABLED = True
 
 REQUIRED_FIELDS = ('arch', 'codename', 'dst', )
